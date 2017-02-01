001b8c3959a0bb683dc711d69c5e9ac77a0189bb
Alexander Midjich <alexander@midjich.se>
2017-02-01 08:20:11 +0100

    added viewport meta tag for responsivnes

--- header.php
+++ header.php
@@ -2,7 +2,8 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Alex Portfolio</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My Portfolio</title>
 	<?php wp_head(); ?>
 </head>
 
